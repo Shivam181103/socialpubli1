4325e9adbea96d7227bfa26512041f84de184b6c
Shivam181103 <shivamchaudharyclassic@gmail.com>
2022-05-30 13:22:39 +0530

    gjhgjh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,78 @@ import {GoLocation} from 'react-icons/go'
 import {BsFillTelephoneFill} from 'react-icons/bs'
 import {MdEmail} from 'react-icons/md'
 import './footer.css'
+import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div style={{backgroundColor:'black',color:'white'}} className='footer '>  
-          <div className="row m-0 footer-wrapper">
+          
+          <footer class="footer-distributed">
+
+			<div class="footer-left">
+
+      <Link className="navbar-brand" to="/">
+                <img style={{width:'9rem'}} src={logo} alt=".."/>
+                </Link>
+
+				<p class="footer-links">
+					<Link to="/" class="link-1">Home</Link>
+					
+					<Link to="/services">Services</Link>
+				
+					<Link to="/influencer">Work</Link>
+				
+					<Link to="/brands">Brands</Link>
+					
+					<Link to="/tools">Tools</Link>
+					
+					<Link to="/about">About</Link>
+
+          <Link to='/contactus'>Contact</Link>
+				</p>
+
+				<p class="footer-company-name"> SocialPubli © 2022</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>1007 One Indiabulls Park , Jetalpur Road</span>Vadodara , Gujarat</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>9723232454</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:influencermarketing@soicalpubli.co.in" style={{color:'white'}}>influencermarketing@soicalpubli.co.in</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About the company</span>
+					Socialpubli is india's fastest growing influencer marketing. We use latest technologies and AI driven decisions to generate higher engagements.
+				</p>
+
+				<div class="footer-icons">
+
+					<a className='face' href="#"><FaFacebookF/></a>
+					<a className='twitt' href="#"><FaTwitter/></a>
+					<a className='linkd' href="https://www.linkedin.com/company/socialpubli-influencer-marketing-company/"><FaLinkedinIn/></a>
+					<a className='insta' href="https://instagram.com/socialpublindia?igshid=YmMyMTA2M2Y="><FaInstagram/></a>
+
+				</div>
+
+			</div>
+
+		</footer>
+          
+          {/* <div className="row m-0 footer-wrapper">
               <div className="col col-lg-4 col-md-6 col-sm-12 col-xm-12">
                   <img style={{width:'9rem'}} src={logo} alt="" />
                   <div className="page-links">
@@ -44,7 +112,7 @@ const Footer = () => {
                       </a>
                   </div>
               </div>
-          </div>
+          </div> */}
     </div>
   )
 }
--- src/components/Home/HomeLanding.jsx
+++ src/components/Home/HomeLanding.jsx
@@ -20,19 +20,22 @@ import brand4 from '../../IMAGES1/brand_4.jpg';
 import brand5 from '../../IMAGES1/brand_5.jpg';
 import Navbar from './Navbar'
 import Slider from "react-slick";
+ 
+import {Link} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 const HomeLanding = () => {
 
     const navigate = useNavigate();
     const [restImg, setrestImg] = useState([talentImg, marketingService]);
-    const [activeImg, setActiveImg] = useState(plateformTechImg);
+    const [activeImg, setActiveImg] = useState(marketingService);
     const [influencerrestImg, setinfluencerrestImg] = useState([brand1, brand2, brand3, brand4]);
     const [influenceractiveImg, setinfluenceractiveImg] = useState(brand5);
-
+const [globalTextClass, setglobalTextClass] = useState('');
 
 
     const changeUI = (e) => {
 
+        
         if (e.target.id === 'first') {
             setrestImg(
                 [talentImg, plateformTechImg]
@@ -52,52 +55,96 @@ const HomeLanding = () => {
             setActiveImg(plateformTechImg)
         }
     }
+    const settings1 = {
+        dots:true,
+        arrows:false,
+        infinite:true,
+        slidesToShow:4,
+        slidesToScroll:1,
+        swipeToSlide: true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                initialSlide: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    }
     const settings = {
         dots: true,
+        arrows:false,
         infinite: true,
-        speed: 500,
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+        swipeToSlide: true,
+        beforeChange: function (currentSlide, nextSlide) {
+            console.log("before change", currentSlide, nextSlide);
+        },
+        afterChange: function (currentSlide) {
+            console.log("after change", currentSlide);
+        }
     };
     return (
         <>
             <div className="home-landing-container">
-                <Navbar  />
+                <Navbar />
                 <div className="landing-page row m-0">
                     <div className="col col-mg-6 col-sm-6 col-xm-12  brand-page-cont">
                         <h1> I AM <br /> BRAND</h1>
-                        <button onClick={()=>navigate("/brandform")} >Click Here</button>
+                        <button onClick={() => navigate("/brandform")} >Click Here</button>
                     </div>
                     <div className="col col-mg-6 col-sm-6 col-xm-12  influencer-page-cont">
                         <h1> I AM  INFLUENCER</h1>
-                        <button onClick={()=>navigate("/influencerform")}>Click Here</button>
+                        <button onClick={() => navigate("/influencerform")}>Click Here</button>
                     </div>
                 </div>
             </div>
             <div id="carouselExampleIndicators" class=" carousel slide" data-bs-ride="carousel">
-                
+
                 <div class="carousel-inner">
-                   
-                    
+
+
                     <div class="carousel-item active">
                         <div className="quote-wrapper m-0 d-flex justify-content-center align-items-center  ">
-                           {
-                               window.innerWidth <= 625 ?
-                               <p >
-                               <img className='left' src={leftqoutemark} alt="" />
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos culpa ab vel? Itaque, alias.  ipsum dolor sit amet consectetur adipisicing elit. Illum placeat nam id voluptatum autem voluptate dignissimos, ipsa dolore eos voluptatibus, sint cumque ut error saepe consequuntur deserunt quod harum. Pariatur.
-                               <img className='right' src={rightqoutemark} alt="" />
+                            {
+                                window.innerWidth <= 625 ?
+                                    <p >
+                                        <img className='left' src={leftqoutemark} alt="" />
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos culpa ab vel? Itaque, alias.  ipsum dolor sit amet consectetur adipisicing elit. Illum placeat nam id voluptatum autem voluptate dignissimos, ipsa dolore eos voluptatibus, sint cumque ut error saepe consequuntur deserunt quod harum. Pariatur.
+                                        <img className='right' src={rightqoutemark} alt="" />
                                     </p>
-                               :
-                               <>
-                               <img className='left' src={leftqoutemark} alt="" />
-                               <p >
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos culpa ab vel? Itaque, alias.  ipsum dolor sit amet consectetur adipisicing elit. Illum placeat nam id voluptatum autem voluptate dignissimos, ipsa dolore eos voluptatibus, sint cumque ut error saepe consequuntur deserunt quod harum. Pariatur.
-                                    </p>
-                               <img className='right' src={rightqoutemark} alt="" />
-                               </>
-                           }
+                                    :
+                                    <>
+                                        <img className='left' src={leftqoutemark} alt="" />
+                                        <p >
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos culpa ab vel? Itaque, alias.  ipsum dolor sit amet consectetur adipisicing elit. Illum placeat nam id voluptatum autem voluptate dignissimos, ipsa dolore eos voluptatibus, sint cumque ut error saepe consequuntur deserunt quod harum. Pariatur.
+                                        </p>
+                                        <img className='right' src={rightqoutemark} alt="" />
+                                    </>
+                            }
                         </div>
                     </div>
                 </div>
@@ -114,36 +161,58 @@ const HomeLanding = () => {
                 </div>
             </div>
             <div className="plateform-wrapper">
-                <div className="global  ">
-                    <div className="global-img-cont">
-                        <img className='first' src={restImg[0]} alt="" />
-                        <img className='second' src={restImg[1]} alt="" />
-                        <img className='third' src={activeImg} alt="" />
-                    </div>
-                    <div className="global-text-cont">
-                        <div id='first' onMouseOver={changeUI} defaultValue='marketing' className="text-cont">
-                            <h1 id='first' data-text='MARKETING SERVICE' >MARKETING SERVICE</h1>
+                <div className="global  "> 
+                {/* <Slider {...settings}> */}
+          <div>
+             
+                 <img src={activeImg} alt="" />
+             
+          </div>
+          {/* <div>
+          <img src={activeImg} alt="" />
+          </div>
+          <div>
+          <img src={activeImg} alt="" />
+          </div>
+         </Slider> */}
 
+
+
+                    <div className="global-text-cont ">
+                        <div id='first' onClick={changeUI} defaultValue='marketing' className="text-cont">
+                            <h1 className={globalTextClass}  id='first' data-text='MARKETING SERVICE' >MARKETING SERVICE</h1>
+                            {
+                                activeImg === marketingService&& 
+                                <div> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nostrum distinctio, cum quae dicta repellat blanditiis ipsam consectetur ex voluptas?</p></div>
+                            }
                         </div>
-                        <div id='second' onMouseOver={changeUI} defaultValue='talent' className="text-cont">
-                            <h1 id='second' data-text='TALENT SERVICE'>TALENT SERVICE</h1>
+                        <div style={{width:'100%'}} id='second' onClick={changeUI} defaultValue='talent' className="text-cont">
+                            <h1  className={globalTextClass}  id='second' data-text='TALENT SERVICE'>TALENT SERVICE</h1>
+                            {
+                                activeImg === talentImg && 
+                                <div> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nostrum distinctio, cum quae dicta repellat blanditiis ipsam consectetur ex voluptas?</p></div>
+                            }
                         </div>
-                        <div id='third' onMouseOver={changeUI} defaultValue='tech' className="text-cont">
-                            <h1 id='third' data-text='PLATEFORM & TECH' >PLATEFORM & TECH</h1>
+                        <div id='third' onClick={changeUI} defaultValue='tech' className="text-cont">
+                            <h1  className={globalTextClass} id='third' data-text='PLATEFORM & TECH' >PLATEFORM & TECH</h1>
+                            {
+                                activeImg === plateformTechImg&& 
+                                <div> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nostrum distinctio, cum quae dicta repellat blanditiis ipsam consectetur ex voluptas?</p></div>
+                            }
                         </div>
                     </div>
                 </div>
             </div>
             <div className="big-heading-links-wrapper">
-                
+
                 <div className="big-heading-link-cont">
-                  <div> <div className='background-heading'> <h1 >INFLUENCER MARKETING</h1></div>  <h1 data-text="INFLUENCER MARKETING" className="big-heading">INFLUENCER MARKETING</h1></div>
-                  <div> <div className='background-heading'>  <h1  >SOCIAL EXPERIMENTAL</h1></div>   <h1 data-text="SOCIAL EXPERIMENTAL" className="big-heading">SOCIAL EXPERIMENTAL</h1></div>
-                  <div><div className='background-heading'> <h1  >PERFORMANCE MARKETING & MEDIA</h1></div>  <h1 data-text="PERFORMANCE MARKETING & MEDIA" className="big-heading">PERFORMANCE MARKETING & MEDIA</h1></div>
-                  <div> <div className='background-heading'> <h1  >CREATIVE CONTENT</h1> </div>  <h1 data-text="CREATIVE CONTENT" className="big-heading">CREATIVE CONTENT</h1></div>
-                  <div><div className='background-heading'> <h1  >RACKING & MEASURMENTS WITH INFLUSOFT</h1> </div>   <h1 data-text="TRACKING & MEASURMENTS WITH INFLUSOFT" className="big-heading">TRACKING & MEASURMENTS WITH INFLUSOFT</h1></div>
-                  <div><div className='background-heading'> <h1  >STRATEGY</h1> </div>  <h1 data-text="STRATEGY" className="big-heading"> STRATEGY</h1></div>
-                  <div> <div className='background-heading'><h1  >VIEW ALL..</h1> </div> <h1 data-text="VIEW ALL.." className="big-heading"> VIEW ALL..</h1></div>
+                    <div> <div className='background-heading'> <h1 >INFLUENCER MARKETING</h1></div>  <h1 data-text="INFLUENCER MARKETING" className="big-heading">INFLUENCER MARKETING</h1></div>
+                    <div> <div className='background-heading'>  <h1  >SOCIAL EXPERIMENTAL</h1></div>   <h1 data-text="SOCIAL EXPERIMENTAL" className="big-heading">SOCIAL EXPERIMENTAL</h1></div>
+                    <div><div className='background-heading'> <h1  >PERFORMANCE MARKETING & MEDIA</h1></div>  <h1 data-text="PERFORMANCE MARKETING & MEDIA" className="big-heading">PERFORMANCE MARKETING & MEDIA</h1></div>
+                    <div> <div className='background-heading'> <h1  >CREATIVE CONTENT</h1> </div>  <h1 data-text="CREATIVE CONTENT" className="big-heading">CREATIVE CONTENT</h1></div>
+                    <div><div className='background-heading'> <h1  >RACKING & MEASURMENTS WITH INFLUSOFT</h1> </div>   <h1 data-text="TRACKING & MEASURMENTS WITH INFLUSOFT" className="big-heading">TRACKING & MEASURMENTS WITH INFLUSOFT</h1></div>
+                    <div><div className='background-heading'> <h1  >STRATEGY</h1> </div>  <h1 data-text="STRATEGY" className="big-heading"> STRATEGY</h1></div>
+                    <div> <div className='background-heading'><h1  >VIEW ALL..</h1> </div> <h1 data-text="VIEW ALL.." className="big-heading"> VIEW ALL..</h1></div>
                 </div>
             </div>
             <div className="global-tech-wrapper">
@@ -157,7 +226,7 @@ const HomeLanding = () => {
                 </div>
             </div>
             <div className="our-influencer-wrapper ">
-                <div className="influencers "> 
+                <div className="influencers ">
                     <img className='brand brand1' src={influencerrestImg[0]} alt="" />
                     <img className='brand brand2' src={influencerrestImg[1]} alt="" />
                     <img className='brand brand3' src={influencerrestImg[2]} alt="" />
@@ -194,7 +263,7 @@ const HomeLanding = () => {
                     </div>
                 </div>
                 <div className="brands-cont">
-                    <Slider {...settings}>
+                    <Slider {...settings1}>
                         <div>
                             <div className="brands-img-wrap">
                                 <img src={brand} alt="" />
